d0955e3ffac66062b01f4f290bb0e397a2994a9f
Unknown <pawelkoter@gmail.com>
2017-10-27 21:40:04 +0200

    Make Book api fluent

--- lab_1/BookCollection/src/main/java/pl/pawelkoter/bookCollection/domain/Book.java
+++ lab_1/BookCollection/src/main/java/pl/pawelkoter/bookCollection/domain/Book.java
@@ -17,15 +17,17 @@ public class Book {
         return id;
     }
 
-    public void setId(int id) {
+    public Book setId(int id) {
         this.id = id;
+        return this;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
+    public Book setTitle(String title) {
         this.title = title;
+        return this;
     }
 }
